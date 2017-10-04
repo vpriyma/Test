b88e5ca78de213c191493c00b7345f1f792ae2ea
Vitaliy Priyma <vitalia.priyma@gmail.com>
2017-10-04 15:22:35 +0300

    The first commit

---
+++ EPAM_test/EPAM_test/MainPageObject.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Configuration;
+using log4net;
+using log4net.Config;
+using Assert = NUnit.Framework.Assert;
+
+namespace EPAM_test
+{
+    public class MainPageObject
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(MainPageObject));
+
+        private readonly IWebDriver _driver;
+        private readonly string _baseUrl;
+
+        public MainPageObject(IWebDriver browser)
+        {
+            this._driver = browser;
+            this._driver.Manage().Window.Maximize();
+            this._baseUrl = ConfigurationManager.AppSettings["URL"];
+            PageFactory.InitElements(browser, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "lst-ib")]
+        public IWebElement SearchBox { get; set; }
+
+        [FindsBy(How = How.Name, Using = "btnK")]
+        public IWebElement GoButton { get; set; }
+
+        [FindsBy(How = How.Id, Using = "search")]
+        public IWebElement ResultsCountDiv { get; set; }
+
+        public void Navigate()
+        {
+            this._driver.Navigate().GoToUrl(this._baseUrl);
+            logger.Debug("Go to " + _baseUrl);
+        }
+
+        public void Search(string textToType)
+        {
+            this.SearchBox.Clear();
+            logger.Debug("Clear Search field");
+
+            this.SearchBox.SendKeys(textToType);
+            logger.Debug("SendKeys: " + textToType);
+
+            this.SearchBox.SendKeys(Keys.Tab);
+
+            this.GoButton.Click();
+            logger.Debug("Click Search");
+        }
+
+        public void ValidateResults(string expectedText)
+        {
+            Assert.IsTrue(this.ResultsCountDiv.Text.Contains(expectedText));
+        }
+    }
+}
---
+++ EPAM_test/EPAM_test/Tests.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading;
+using log4net;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace EPAM_test
+{
+    [TestFixture]
+    public class Tests
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Tests));
+
+        private IWebDriver _driver;
+        private StringBuilder _verificationErrors;
+        private WebDriverWait _wait;
+        private MainPageObject _googleMainPageObject;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            _driver = new InternetExplorerDriver();
+            _verificationErrors = new StringBuilder();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            _googleMainPageObject = new MainPageObject(_driver);
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                _driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", _verificationErrors.ToString());
+        }
+
+        [Test]
+        public void Test1()
+        {
+            logger.Info("Start Test1");
+
+            var request = "automation";
+
+            // Open IE and Navigate to google.com.ua 
+            _googleMainPageObject.Navigate();
+
+            //Search for “automation”.
+            _googleMainPageObject.Search(request);
+
+            //Waiting for Search result appearing
+            _wait.Until(ExpectedConditions.ElementExists(By.Id("vs0p1c0")));
+
+            //Open the first link on search results page.
+            _driver.FindElement(By.Id("vs0p1c0")).Click();
+
+            logger.Debug("Click on the first link in Search result");
+
+            //Verify that title contains searched word
+            try
+            {
+                Assert.IsTrue(_driver.Title.ToLower().Contains(request));
+                logger.Info("Test1 is PASSED.");
+            }
+            catch (Exception e)
+            {
+                logger.Debug("Test1 is FAILED! Title doesn't contain title " + request + " Error: " + e.Message + DateTime.Now);
+            }
+            
+        }
+
+        [Test]
+        public void Test2()
+        {
+            logger.Info("Start Test2");
+
+            var request = "automation";
+            var expectedText = "testautomationday.com";
+
+            // Open IE and Navigate to google.com.ua 
+            _googleMainPageObject.Navigate();
+
+            //Search for “automation”.
+            _googleMainPageObject.Search(request);
+
+            //Searching "testautomationday.com" in search results of 1, 2, 3, 4, 5 pages
+            logger.Info("Check page #1");
+            try
+            {
+                _googleMainPageObject.ValidateResults(expectedText);
+                logger.Debug("True");
+            }
+            catch (AssertionException e)
+            {
+                _verificationErrors.Append("1 page: " + e.Message);
+                logger.Debug("False");
+            }
+
+            logger.Info("Check page #2");
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText("2")));
+            _driver.FindElement(By.LinkText("2")).Click();
+            try
+            {
+                _googleMainPageObject.ValidateResults(expectedText);
+                logger.Debug("True");
+            }
+            catch (AssertionException e)
+            {
+                _verificationErrors.Append("2 page: " + e.Message);
+                logger.Debug("False");
+            }
+
+            logger.Info("Check page #3");
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText("3")));
+            _driver.FindElement(By.LinkText("3")).Click();
+            try
+            {
+                _googleMainPageObject.ValidateResults(expectedText);
+                logger.Debug("True");
+            }
+            catch (AssertionException e)
+            {
+                _verificationErrors.Append("3 page: " + e.Message);
+                logger.Debug("False");
+            }
+
+            logger.Info("Check page #4");
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText("4")));
+            _driver.FindElement(By.LinkText("4")).Click();
+            try
+            {
+                _googleMainPageObject.ValidateResults(expectedText);
+                logger.Debug("True");
+            }
+            catch (AssertionException e)
+            {
+                _verificationErrors.Append("4 page: " + e.Message);
+                logger.Debug("False");
+            }
+
+            logger.Info("Check page #5");
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText("5")));
+            _driver.FindElement(By.LinkText("5")).Click();
+            try
+            {
+                _googleMainPageObject.ValidateResults(expectedText);
+                logger.Debug("True");
+            }
+            catch (AssertionException e)
+            {
+                _verificationErrors.Append("5 page: " + e.Message);
+                logger.Debug("False");
+            }
+        }
+    }
+}
